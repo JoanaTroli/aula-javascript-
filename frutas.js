ffa2763e6196e719db061b37cfe41cc9ec325178
JoanaTroli <joanatroli674@gmail.com>
2024-10-07 21:28:01 -0300

    exercicio-html

--- frutas.js
+++ frutas.js
@@ -3,3 +3,4 @@ const frutas = ["maca", "manga", "morango", "melancia", "maracuja", "acerola"];
 console.log("A fruta na posição 1 é: ", frutas[1]);
 console.log("A fruta na posição 4 é: ", frutas[4]);
 console.log("Meu array tem ", frutas.length, "frutas");
+console.log("Última fruta:", frutas[frutas.length -1]); 
